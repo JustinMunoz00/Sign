0a4f1b22edc10e8982585d139d4dc8a4a8e00689
JustinMunoz00 <justinm880@nycstudents.net>
2025-02-06 16:49:28 +0000

    Doing test case

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        String str; 
+        int x;
+        Sign sign1 = new Sign("ABC222DE", 3);
+        x = sign1.numberOfLines();
+        System.out.println(x);
+        str = sign1.getLines();
+        System.out.println(str);
+    }
+}
---
+++ Sign.java
@@ -0,0 +1,22 @@
+public class Sign {
+    private String message;
+    private int width; 
+    public Sign(String m, int w){
+        message = m;
+        width = w;
+    }
+    public int numberOfLines(){
+        int lines = 0;
+        lines += message.length()/width;
+        if (message.length() % width > 0) lines++;
+        return lines;
+    }
+    public String getLines(){
+        int lines = numberOfLines();
+        String s = message;
+        for (int i = 1; i < lines; i++){
+            s = s.substring(0, i*width+i-1) + ";" + s.substring(i*width+i-1);
+        }
+        return s;
+    }
+}
